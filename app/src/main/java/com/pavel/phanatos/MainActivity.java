3dc11c4722fb25f9e6b5189978606242e15296f8
user <3214>
2023-02-17 19:53:20 +0300

    add todo for george

--- app/src/main/java/com/pavel/phanatos/MainActivity.java
+++ app/src/main/java/com/pavel/phanatos/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //todo: add adapter to  tabitem / add frames for fragments and swith
+
     }
 }
--- app/src/main/java/com/pavel/phanatos/MainFrame.java
+++
@@ -1,5 +0,0 @@
-package com.pavel.phanatos;
-
-public class MainFrame {
-
-}
