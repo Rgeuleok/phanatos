dc7ddd37f14b152552042ee27d869fa4e0434b8b
george <georgyfilatov@yandex.ru>
2023-02-24 23:29:13 +0300

    add scroll tab layout

--- app/src/main/java/com/pavel/phanatos/MainActivity.java
+++ app/src/main/java/com/pavel/phanatos/MainActivity.java
@@ -1,17 +1,54 @@
 package com.pavel.phanatos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    MainViewPagerAdapter mainViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //todo: add adapter to  tabitem / add frames for fragments and swith
+        tabLayout = findViewById(R.id.mainTabLayout);
+        viewPager2 = findViewById(R.id.mainViewPager);
+        mainViewPagerAdapter = new MainViewPagerAdapter(this);
+        viewPager2.setAdapter(mainViewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                Objects.requireNonNull(tabLayout.getTabAt(position)).select();
+            }
+        });
 
     }
 }
